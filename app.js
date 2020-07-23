459e3827c9a6425f22c7d3bc1e2ef3fdb377a288
Allana Anderson <allana.anderson17@gmail.com>
2020-07-23 08:05:25 -0600

    moved inquirer prompts from subclasses to app.js and created create functions for each team member.

--- app.js
+++ app.js
@@ -12,32 +12,133 @@ const render = require("./lib/htmlRenderer");
 
 //This array will hold each new team member object that is created and will be passed to the render function.
 const teamMembers = [];
-const noMoreMembers = false;
 
-const manager = new Manager(getName(), getId(), getEmail(), getOfficeNumber());
-teamMembers.push(manager);
+function createManager(){
+    console.log("Please enter your team's information.")
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your employee's name?",
+            name: "name"
 
-while (!noMoreMembers){
+        },
+        {
+            type: "number",
+            message: "What is your employee's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your employee's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your manager's office number?",
+            name: "officeNumber"
+        }
+
+    ]).then((response)=>{
+        const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+        teamMembers.push(manager);
+        createTeam();
+    })
+}
+function createEngineer(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your employee's name?",
+            name: "name"
+
+        },
+        {
+            type: "number",
+            message: "What is your employee's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your employee's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your GitHub username?",
+            name: "github"
+        }
+
+    ]).then((response)=>{
+        const engineer = new Engineer(response.name, response.id, response.email, response.github);
+        teamMembers.push(engineer);
+        createTeam();
+    })
+}
+function createIntern(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your employee's name?",
+            name: "name"
+
+        },
+        {
+            type: "number",
+            message: "What is your employee's ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your employee's email?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your intern's school?",
+            name: "school"
+        }
+
+    ]).then((response)=>{
+        const intern = new Intern(response.name, response.id, response.email, response.school);
+        teamMembers.push(intern);
+        createTeam();
+    })
+}
+
+function createTeam(){
     inquirer.prompt([
         {
             type: "list",
-            question: "Which type of team member would you like to add?",
+            message: "Which type of team member would you like to add?",
             name: "role",
             choices: ["Engineer", "Intern", "I don't want to add any more team members"]
         }
-    ]);
-    if(this.role === "Engineer"){
-        const engineer = new Engineer(getName(), getId(), getEmail(), getGitHub());
-    }else if(this.role === "Intern"){
-        const intern = new Intern(getName(), getId(), getEmail(), getSchool());
-    }else{
-        noMoreMembers = true;
-    }
+    ]).then((response)=>{
+        switch(response.role){
+            case "Engineer":
+                createEngineer();
+                break;
+            case "Intern":
+                createIntern();
+                break;
+            default:
+                checkOutPut();
+        }
+    })
+
 }
 
-const teamHTML = render(teamMembers);
 
-fs.writeFileSync("team.html", teamHTML);
+createManager();
+
+function checkOutPut(){
+    if (!fs.existsSync(OUTPUT_DIR)){
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+} 
+
 
 
 
