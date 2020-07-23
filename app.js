abbfe9adece9e9b5c03181efa4348496a6caae43
Allana Anderson <allana.anderson17@gmail.com>
2020-07-23 15:23:26 -0600

    added comment to app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//requiring subclasses and node platforms
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,6 +6,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+//creating output path
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -13,12 +15,13 @@ const render = require("./lib/htmlRenderer");
 //This array will hold each new team member object that is created and will be passed to the render function.
 const teamMembers = [];
 
+//This function starts off with the user entering information about their manager then calls createTeam
 function createManager(){
     console.log("Please enter your team's information.")
     inquirer.prompt([
         {
             type: "input",
-            message: "What is your employee's name?",
+            message: "What is your Manager's name?",
             name: "name"
 
         },
@@ -39,11 +42,15 @@ function createManager(){
         }
 
     ]).then((response)=>{
+        // information is stored in a new Manager object
         const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+        //manager pushed to teamMembers array
         teamMembers.push(manager);
         createTeam();
     })
 }
+
+//This function starts off with the user entering information about the Engineer then calls createTeam
 function createEngineer(){
     inquirer.prompt([
         {
@@ -69,11 +76,14 @@ function createEngineer(){
         }
 
     ]).then((response)=>{
+        // information is stored in a new Engineer object
         const engineer = new Engineer(response.name, response.id, response.email, response.github);
         teamMembers.push(engineer);
         createTeam();
     })
 }
+
+//This function starts off with the user entering information about the Engineer then calls createTeam
 function createIntern(){
     inquirer.prompt([
         {
@@ -99,12 +109,14 @@ function createIntern(){
         }
 
     ]).then((response)=>{
+        // information is stored in a new Intern object
         const intern = new Intern(response.name, response.id, response.email, response.school);
         teamMembers.push(intern);
         createTeam();
     })
 }
 
+//prompts the user for what type of employee they would like to add
 function createTeam(){
     inquirer.prompt([
         {
@@ -114,6 +126,7 @@ function createTeam(){
             choices: ["Engineer", "Intern", "I don't want to add any more team members"]
         }
     ]).then((response)=>{
+        //switch case that creates each type of subclass and calls checkOutPut when the user is done
         switch(response.role){
             case "Engineer":
                 createEngineer();
@@ -125,12 +138,9 @@ function createTeam(){
                 checkOutPut();
         }
     })
-
 }
 
-
-createManager();
-
+//checks if folder for output exists and if it does not, it creates one. Writes HTML files into this folder.
 function checkOutPut(){
     if (!fs.existsSync(OUTPUT_DIR)){
         fs.mkdirSync(OUTPUT_DIR);
@@ -140,7 +150,12 @@ function checkOutPut(){
 } 
 
 
-// Add comments
+createManager();
+
+
+
+
+
 // Make ReadME
 //Add message to let user know they are entering info on a manager
 //check that user actually entered something
