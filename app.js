2db64fcbd9e73999f567ea357024cf338451bdcc
Allana Anderson <allana.anderson17@gmail.com>
2020-07-23 08:06:52 -0600

    added notes to let me know what I have left to do

--- app.js
+++ app.js
@@ -140,35 +140,7 @@ function checkOutPut(){
 } 
 
 
-
-
-
-//*empty array
-//* fill empty array with objects for each team member - HOW DO I GET THIS INFO
-//* Boolean operator - while no more team members is false, keep asking 
-//* when user hits "I don't want..." call the render function; pass array of objects
-//* ^ This will produce your HTML, Use it to create an HTML file named team.html in the output folder. Use output path to target this location.
-//TODO ^^ HINT check if output folder exists. If not, create it.
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// Add comments
+// Make ReadME
+//Add message to let user know they are entering info on a manager
+//check that user actually entered something
